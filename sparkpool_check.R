414a6a4bfc27f8f8e03fca51616712ac0ddefad8
hctung101 <33676894+hctung101@users.noreply.github.com>
2021-05-28 16:33:53 +0800

    Add to TA2

--- sparkpool_check.R
+++ sparkpool_check.R
@@ -1,6 +1,6 @@
 options( java.parameters = "-Xmx6g" )
 for(i in 1:10){
-  if(14 == length(list.files("/data/kent/sparkpool/", pattern = gsub("-| |:","", Sys.Date())))){
+  if(15 == length(list.files("/data/kent/sparkpool/", pattern = gsub("-| |:","", Sys.Date())))){
     print("----------Check ok----------")
     break
   }else{
